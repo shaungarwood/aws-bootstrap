d10e26ba7ac13254b03df686eafb487268749de7
Shaun Garwood <shaun.garwood@gmail.com>
2025-02-02 21:48:20 -0700

    documentation

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "iam_user_name" {
   description = "The name of the IAM user"
   type        = string
-  default     = "csw"
+  default     = "csw" # This is "censorshipwreck" as a quick username. Please change as needed.
 }
